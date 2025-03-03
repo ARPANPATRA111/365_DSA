602791f442e5dd4356c9193291a008b0385b79f9
ARPANPATRA111 <arpanpatra91111@gmail.com>
2025-03-03 17:47:42 +0530

    Reverseing a LinkedList question added.

---
+++ Linked-List/reverseLL.java
@@ -0,0 +1,51 @@
+/*LeetCode link:- https://leetcode.com/problems/reverse-linked-list/ */
+
+class reverseLL {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+
+    // Method to create a linked list from an array of values
+    public static ListNode createList(int[] values) {
+        if (values.length == 0) return null;
+        ListNode head = new ListNode(values[0]);
+        ListNode curr = head;
+        for (int i = 1; i < values.length; i++) {
+            curr.next = new ListNode(values[i]);
+            curr = curr.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] values = {1, 2, 3, 4, 5};
+        ListNode head = createList(values);
+        printList(head);
+        head = reverseList(head);  // Update the head to point to the reversed list
+        printList(head);
+    }
+}
